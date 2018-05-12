8a110edfe3c501a2a35eb8cc0111840fc2dda7a0
Kiss <kissdavid0506@gmail.com>
2018-05-12 15:36:35 +0200

    Add package informations

---
+++ FlokeszOwnEngine/src/main/java/com/mycompany/flokeszownengine/BL/package-info.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2018 Kiss Dávid.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Ez a package az uzleti logikat megvalosito osztalyokat tartalmazza.
+ * Bear -> a medve enemy osszes komponense.
+ * Character -> a karakter osszes komponense.
+ * GameEngine -> a jatekmotor megvalositasa.
+ * Input -> a billentyuzet leutes erzekelese.
+ * Menu -> a menu megjelenitese.
+ * Stage1 -> a mezo futtatasa.
+ * Weapon -> a fegyver osszes komponense.
+ */
+package com.mycompany.flokeszownengine.BL;
